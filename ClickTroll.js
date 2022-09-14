5a6a8be552da64ad2b862f2658aa7e9d00f7d8e2
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-09-14 11:49:05 -0500

    my attempt with creating one of the bookmarklets of 2022

--- ClickTroll.js
+++ ClickTroll.js
@@ -1,7 +1,11 @@
 document.body.onclick = function () {
-  a = localStorage.getItem("oasdijwoidawid");
-  if (!localStorage.getItem("oasdijwoidawid")) a = "TROLLED!!!!!";
-  var e = "https://realmafricasafaris.com/wp-content/uploads/2020/02/The-Shoebill-Stork.jpg";
+  a = JSON.parse(localStorage.getItem("studio")).ct_msg;
+  if (!JSON.parse(localStorage.getItem("studio")).ct_msg) {
+    a.ct_msg = "TROLLED!!!!!";
+    a.ct_url = "https://realmafricasafaris.com/wp-content/uploads/2020/02/The-Shoebill-Stork.jpg";
+    localStorage.setItem("studio", JSON.stringify(a))
+  }
+  var e = JSON.parse(localStorage.getItem("studio")).ct_url;
   o = document.createElement("iframe");
   o.width = 1280;
   o.height = 720;
--- Studio.js
+++ Studio.js
@@ -1,1 +1,5 @@
-// In Studio you can tweak options never seen before
+// In Studio you can tweak options never seen before in Clicktroll or any other bookmarklet !
+if(!localStorage.getItem("studio")) {
+    alert("Studio has never been configured on this page! Configuring..")
+    localStorage.setItem("studio",{ct_msg:"TROLLED!!!!!",ct_url:"https://realmafricasafaris.com/wp-content/uploads/2020/02/The-Shoebill-Stork.jpg"})
+}
