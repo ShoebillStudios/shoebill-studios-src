430be77e21eed7ba5ee7d8c5202769b18b1a0f27
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-09-14 11:34:01 -0500

    will format soon

---
+++ ClickTroll-1.0.js
@@ -0,0 +1,4 @@
+document.body.onclick=function(){
+  a=localStorage.getItem("oasdijwoidawid")
+                                 localStorage.getItem("oasdijwoidawid")||(a="TROLLED!!!!!");var e="https://realmafricasafaris.com/wp-content/uploads/2020/02/The-Shoebill-Stork.jpg";o=document.createElement("iframe"),o.width=1280,o.height=720,o.src=e,document.body.appendChild(o),ol={x:window.screenX,y:window.screenY},scrollTo(0,9999999999999),alert(a)
+                                 setTimeout(function(){scrollTo(ol.x,ol.y),o.remove()},1500)
