e98bb100009473a3901389ee192f01000f2eee8e
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-09-14 11:51:47 -0500

    compiled bookmarklets

--- Studio.js
+++ Studio.js
@@ -1,5 +1,6 @@
 // In Studio you can tweak options never seen before in Clicktroll or any other bookmarklet !
+var defaultJson = {ct_msg:"TROLLED!!!!!",ct_url:"https://realmafricasafaris.com/wp-content/uploads/2020/02/The-Shoebill-Stork.jpg"}
 if(!localStorage.getItem("studio")) {
     alert("Studio has never been configured on this page! Configuring..")
-    localStorage.setItem("studio",{ct_msg:"TROLLED!!!!!",ct_url:"https://realmafricasafaris.com/wp-content/uploads/2020/02/The-Shoebill-Stork.jpg"})
+    localStorage.setItem("studio",defaultJson)
 }
