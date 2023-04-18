8d78527d88e9150a265f05dc51d32e437f788c9a
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-18 16:20:19 -0500

    Update ClickTroll-MessageEditor.js

--- ClickTroll-MessageEditor.js
+++ ClickTroll-MessageEditor.js
@@ -1,2 +1,2 @@
-a = prompt("Message");
-localStorage.setItem("oasdijwoidawid", a);
+var toBeSet = prompt("Set your message here");
+localStorage.setItem("CT:MTY4MTg1Mjc3NDo0LzE4LzIzOkN0djI=", toBeSet);
