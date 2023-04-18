a31de0bcb7af3eec1c630fce6668ca28e86e5cee
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-18 16:19:07 -0500

    old project never got around to doing

--- Studio.js
+++
@@ -1,6 +0,0 @@
-// In Studio you can tweak options never seen before in Clicktroll or any other bookmarklet !
-var defaultJson = {ct_msg:"TROLLED!!!!!",ct_url:"https://realmafricasafaris.com/wp-content/uploads/2020/02/The-Shoebill-Stork.jpg"}
-if(!localStorage.getItem("studio")) {
-    alert("Studio has never been configured on this page! Configuring..")
-    localStorage.setItem("studio",defaultJson)
-}
