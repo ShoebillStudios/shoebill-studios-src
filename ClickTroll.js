77f982d6447f3f238f14ab23b5ac773c558c8a01
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-18 16:22:16 -0500

    Update ClickTroll.js

--- ClickTroll.js
+++ ClickTroll.js
@@ -1,21 +1,20 @@
 document.body.onclick = function () {
-  a = JSON.parse(localStorage.getItem("studio")).ct_msg;
-  if (!JSON.parse(localStorage.getItem("studio")).ct_msg) {
-    a.ct_msg = "TROLLED!!!!!";
-    a.ct_url = "https://realmafricasafaris.com/wp-content/uploads/2020/02/The-Shoebill-Stork.jpg";
-    localStorage.setItem("studio", JSON.stringify(a))
+  ct_message = localStorage.getItem("CT:MTY4MTg1Mjc3NDo0LzE4LzIzOkN0djI=");
+  ct_url = "https://realmafricasafaris.com/wp-content/uploads/2020/02/The-Shoebill-Stork.jpg";
+  if (!localStorage.getItem("CT:MTY4MTg1Mjc3NDo0LzE4LzIzOkN0djI=")) {
+    ct_message = "TROLLED!!!!";
+    localStorage.setItem("CT:MTY4MTg1Mjc3NDo0LzE4LzIzOkN0djI=", ct_message);
   }
-  var e = JSON.parse(localStorage.getItem("studio")).ct_url;
   o = document.createElement("iframe");
   o.width = 1280;
   o.height = 720;
-  o.src = e;
+  o.src = ct_url;
   document.body.appendChild(o);
   ol = { x: window.screenX, y: window.screenY };
   scrollTo(0, 9999999999999);
-  alert(a);
+  alert(ct_message);
   setTimeout(function () {
     scrollTo(ol.x, ol.y);
     o.remove();
-  }, 1500);
+  }, 250);
 };
